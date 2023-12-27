2331432b2ee22e0fa1e9fabd49f12b187673b5e2
Rajagurub <balasrkrg1969@gmail.com>
2023-12-27 08:16:27 +0000

    add page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-
+import Emp_login from "./pages/Emp_login"
 const App=()=>{
   return(
     <BrowserRouter>
-      
+      <Emp_login/>
     </BrowserRouter>
   )
 }
