1d5a0cdd53673ab586aa0f6ca45802f9d2680d81
Rajagurub <balasrkrg1969@gmail.com>
2023-12-27 08:30:32 +0000

    token

--- src/pages/Emp_login.js
+++ src/pages/Emp_login.js
@@ -1,16 +1,28 @@
 import {Input} from "../components";
 const Emp_login=()=>{
     let dummyObj={name:"",pws:""};
-    let onChFun=(val)=>{
+    const  onChFun=(val)=>{
         dummyObj.name=val;
     }
+    const onChfunPws=(val)=>{
+        dummyObj.pws=val;
+    }
+    const submitForm=()=>{
+     if(dummyObj.name!==""&&dummyObj.pws!==""&&dummyObj.name==="employee1"&&dummyObj.pws==="emp@123"){
+       let token= Math.random().toString(36);
+       localStorage.setItem("token",token );
+       localStorage.setItem("type",emp );
+     }
+    }
+    console.log(dummyObj,"dummyObj")
     return(
         <div>
    <form>
     <h2>{"login Form"}</h2>
     <Input initVal={dummyObj.name} inputType={"text"} placeHolder={"user name"} handelChangeFun={onChFun} />
+    <Input initVal={dummyObj.pws} inputType={"password"} placeHolder={"password"} handelChangeFun={onChfunPws} />
 </form>
-<button>{"submit"}</button>
+<button onSubmit={submitForm}>{"submit"}</button>
         </div>
    
     )
